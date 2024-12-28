d0132acf8b038566435c14546fc887c195b7834c
Zahran Dhar <zahrannazirdhar@gmail.com>
2024-12-28 13:23:25 +0530

    LCU

--- Leetcode_Problems/ReverseInteger.c
+++ Leetcode_Problems/ReverseInteger.c
@@ -4,16 +4,15 @@ https://leetcode.com/problems/reverse-integer/description/
 
 int reverse(int x){
 
-    long int y=0;
+    int y=0;
     
     while(x!=0)
     {
+        
+        if((y<(INT_MIN/10))||((y==(INT_MIN/10))&&((x%10)<(INT_MIN%10)))) return 0;
+        if((y>(INT_MAX/10))||((y==(INT_MAX/10))&&((x%10)>(INT_MAX%10)))) return 0;
+      
         y=(y*10+x%10);
-        if(y<-(pow(2,31))||y>pow(2,31))
-        {
-            y=0;
-            break;
-        }
         x=x/10;
     }
 
