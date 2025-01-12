a2f3cf43946ba8f09a816680e6a7e51747f252ad
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-01-12 11:53:46 +0530

    LPU

--- Leetcode_Problems/TwoSum.c
+++ Leetcode_Problems/TwoSum.c
@@ -12,7 +12,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 
     for(i=0;i<(numsSize-1);i++)
     {
-        for(j=(i+1);j<=(numsSize-1);j++)
+        for(j=(i+1);j<numsSize;j++)
         {
             if((nums[i]+nums[j])==target) 
             {
