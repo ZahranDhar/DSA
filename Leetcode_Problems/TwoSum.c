df98c815a29140ea88310fab7a62cc080705a996
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-01-12 11:47:59 +0530

    LPU

--- Leetcode_Problems/TwoSum.c
+++ Leetcode_Problems/TwoSum.c
@@ -6,23 +6,22 @@ https://leetcode.com/problems/two-sum/description/
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int i,j,indices_added=0;
+    int i,j;
     int *SumArr=malloc(2*(sizeof(int)));
+    *returnSize=2;
 
     for(i=0;i<(numsSize-1);i++)
     {
-        if(indices_added) break;
         for(j=(i+1);j<=(numsSize-1);j++)
         {
             if((nums[i]+nums[j])==target) 
             {
                 SumArr[0]=i;
                 SumArr[1]=j;
-                indices_added=1;
-                break;
+                return SumArr;
             }
         }
     }
-    *returnSize=2;
-    return SumArr;
+    
+    return NULL;
 }
